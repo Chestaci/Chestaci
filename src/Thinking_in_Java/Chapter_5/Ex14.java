a9bb74e02e4153bfb26a1e4a4f72d0277fb4ca71
Chestaci <chestaci@gmail.com>
2018-02-21 18:23:10 +0400

    Commit first

--- src/Thinking_in_Java/Chapter_5/Ex14.java
+++ src/Thinking_in_Java/Chapter_5/Ex14.java
@@ -4,7 +4,8 @@ public class Ex14 {
     public static void main(String[] args) {
         System.out.println("************main****************");
         Ex14Static ex14 = new Ex14Static();
-        ex14.print();
+       ex14.print();
+     //   ex14Static.print();
     }
     static Ex14Static ex14Static = new Ex14Static();
 }
@@ -13,16 +14,13 @@ class Ex14Static{
     static String s2;
     static{
         s2 = "static String s2";
-        System.out.println(s);
-        System.out.println(s2);
-    }
+        System.out.println(s + " static block");
+        System.out.println(s2 + " static block");
+        }
     static void print(){
-        System.out.println("************print()*************");
-        System.out.println("********************************");
         System.out.println(s);
         System.out.println(s2);
-        System.out.println("********************************");
-    }
+       }
     Ex14Static(){
         System.out.println("Ex14Static()");
     }
---
+++ src/Thinking_in_Java/Chapter_5/Ex15.java
@@ -0,0 +1,26 @@
+package Thinking_in_Java.Chapter_5;
+
+public class Ex15 {
+    public static void main(String[] args) {
+        ExTest exTest2 = new ExTest();
+        exTest2.print();
+    }
+}
+class ExTest{
+    String s;
+    String s2;
+    {
+        s = "non-static s";
+        s2 = "non-static s2";
+        System.out.println("Block initialisation");
+        System.out.println(s +" in block initialisation");
+        System.out.println(s2 +" in block initialisation");
+    }
+    ExTest(){
+        System.out.println("Constructor ExTest");
+    }
+    void print(){
+        System.out.println("print(): " + s);
+        System.out.println("print(): " + s2);
+    }
+}
