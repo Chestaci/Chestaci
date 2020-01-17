6df523d9bd2ea1f7164e35fb03cd83ee019a15d2
Chestaci <chestaci@gmail.com>
2020-01-17 21:43:05 +0300

    Commit first

---
+++ src/Thinking_in_Java/Chapter_18/Ex21.java
@@ -0,0 +1,18 @@
+package Thinking_in_Java.Chapter_18;
+
+import net.mindview.util.TextFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex21 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        String text = TextFile.read("src/Thinking_in_Java/Chapter_18/Ex21.java");
+        String s;
+        while ((s = bufferedReader.readLine()) != null && s.length() != 0){
+            System.out.println(s.toUpperCase());
+        }
+    }
+}
